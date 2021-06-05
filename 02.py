5e4649f68ab90b044bc3497f376c93adc195b3d3
Guilherme Santos <guipinela.santos@gmail.com>
2021-06-05 17:04:29 +0100

    commit

---
+++ 02.py
@@ -0,0 +1,14 @@
+array1 = [True,  True,  True,  False,
+          True,  True,  True,  True,
+          True,  False, True,  False,
+          True,  False, False, True,
+          True,  True,  True,  True,
+          False, False, True,  True]
+
+
+def count_sheeps(sheep):
+    for boolean in array1:
+        print(boolean)
+
+
+count_sheeps(array1)
