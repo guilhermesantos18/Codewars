29d503a844a6fba9cb9e9eb753448ec8af08969f
Guilherme Santos <guipinela.santos@gmail.com>
2021-06-13 11:57:30 +0100

    commit

---
+++ 06.py
@@ -0,0 +1,7 @@
+def repeat_str(repeat, string):
+    print(f'"{string * repeat}"')
+
+
+repeat_str(4, 'a')
+repeat_str(3, 'hello ')
+repeat_str(2, 'abc')
--- teste.py
+++ teste.py
@@ -1,6 +1,2 @@
-frase = 'Ola tudo bem'
-vogais = 'aeiouAOE'
-for pos in range(0, len(vogais)):
-    frase = frase.replace(vogais[pos], '')
-    print(frase)
-print(frase)
+mo = 'Guilherme'
+print('{}'.format(mo))
