3cd8d579b9311acd7595ec35f072e63e2706244a
Guilherme Santos <guipinela.santos@gmail.com>
2021-06-27 15:10:24 +0100

    commit

--- 07.py
+++ 07.py
@@ -1,12 +1,10 @@
 cont = 0
+
+
 def is_valid_walk(walk):
-    lista_pos_n = []
-    lista_pos_s = []
-    lista_pos_w = []
-    lista_pos_e = []
+    lista_pos_sentidos = []
     ida_destino = walk
     voltar_onde_partiu = []
-    entrou = False
     global cont
     # Consegue ir para o destino em 10 min
     if len(ida_destino) == 10:
@@ -14,34 +12,29 @@ def is_valid_walk(walk):
         for sentido in range(len(ida_destino) - 1, -1, -1):
             voltar_onde_partiu.append(ida_destino[sentido])
         for pos in range(0, len(ida_destino)):
-            if ida_destino[pos] == 's':
-                lista_pos_s.append(pos)
-            elif ida_destino[pos] == 'n':
-                lista_pos_n.append(pos)
-            elif ida_destino[pos] == 'w':
-                lista_pos_w.append(pos)
-            elif ida_destino[pos] == 'e':
-                lista_pos_e.append(pos)
-        print(ida_destino)
-        print(voltar_onde_partiu)
-        if lista_pos_s:
-            for pos in lista_pos_s:
-                if ida_destino[pos] == 's' and voltar_onde_partiu[pos] == 'n':
-                    cont += 1
-                    if cont == len(lista_pos_s):
-                        return True
-                        cont = 0
-                    else:
-                        return False
-                        cont = 0
-        elif lista_pos_n:
-            for pos in lista_pos_n:
-                if ida_destino[pos] == 'n' and voltar_onde_partiu[pos] == 's':
-                     return True
-        # else:
-        #     return False
+            if ida_destino[pos] == 's' or 'n' or 'w' or 'e':
+                lista_pos_sentidos.append(pos)
+        # print(ida_destino)
+        # print(voltar_onde_partiu)
+        for sentido in lista_pos_sentidos:
+            if ida_destino[sentido] == 'n' and voltar_onde_partiu[sentido] == 's':
+                cont += 1
+            elif ida_destino[sentido] == 's' and voltar_onde_partiu[sentido] == 'n':
+                cont += 1
+            elif ida_destino[sentido] == 'w' and voltar_onde_partiu[sentido] == 'e':
+                cont += 1
+            elif ida_destino[sentido] == 'e' and voltar_onde_partiu[sentido] == 'w':
+                cont += 1
+        # Se o cont for igual ao comprimento da lista quer dizer que todas as posições estão corretas
+        if cont == len(voltar_onde_partiu):
+            return True
+        else:
+            return False
     else:
         return False
 
 
-print(is_valid_walk(['e', 'w', 'w', 'e', 'n', 's', 'n', 's', 'n', 'w']))
+print(is_valid_walk(['n', 's', 'n', 's', 'n', 's', 'n', 's', 'n', 's']))
+print(is_valid_walk(['w', 'e', 'w', 'e', 'w', 'e', 'w', 'e', 'w', 'e', 'w', 'e']))
+print(is_valid_walk(['w']))
+print(is_valid_walk(['n', 'n', 'n', 's', 'n', 's', 'n', 's', 'n', 's']))
