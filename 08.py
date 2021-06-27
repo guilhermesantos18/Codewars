74ac01304a067364051a52177ea3ae231f32e6be
Guilherme Santos <guipinela.santos@gmail.com>
2021-06-27 19:52:25 +0100

    commit

--- 08.py
+++ 08.py
@@ -1,9 +1,17 @@
 def comp(matriz1, matriz2):
-    num = matriz1[0]
-    res = num ** 2
-    print(res)
+    cont = 0
+    if str(matriz1[-1]**2) in str(matriz2[0]):
+        cont += 1
+    for pos in range(0, len(matriz1) - 1):
+        if str(matriz1[pos]**2) in matriz2:
+            cont += 1
+    print(cont)
+    if cont == len(matriz2):
+        return True
+    else:
+        return False
 
 
 a1 = [121, 144, 19, 161, 19, 144, 19, 11]
-a2 = [121, 14641, 20736, 361, 25921, 361, 20736, 361]
-comp(a1, a2)
+a2 = [11*11, 121*121, 144*144, 190*190, 161*161, 19*19, 144*144, 19*19]
+print(comp(a1, a2))
