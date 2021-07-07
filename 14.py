8bc90b5f2d6aee0bfb0434de1db2a2a84fd3129a
Guilherme Santos <guipinela.santos@gmail.com>
2021-07-07 17:34:24 +0100

    commit

--- 14.py
+++ 14.py
@@ -3,14 +3,15 @@ def expanded_form(num):
     x = len(num) - 1
     res = []
     for n in range(0, len(num) - 1):
-        if num[n] > str(0):
+        if num[n] != str(0):
             forma_expandida = num[n]
             forma_expandida += str(0)*x
             res.append(int(forma_expandida))
             x -= 1
         else:
             x -= 1
-    res.append(int(num[-1]))
+    if num[-1] != str(0):
+        res.append(int(num[-1]))
     res = str(res)
     res = res.replace(',', ' +')
     res = res.replace(']', '')
