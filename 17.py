3ba0d8ef0e38c33c8388f0f4167fb7b16517afe0
Guilherme Santos <guipinela.santos@gmail.com>
2021-07-10 17:10:40 +0100

    commit

--- 17.py
+++ 17.py
@@ -1,9 +1,19 @@
 def longest_consec(strarr, k):
-    x = 0
+    if k > 1:
+        passo = k - 1
+    else:
+        passo = k
+    lista_palavras_juntas = []
     if not strarr or k > len(strarr) or k <= 0:
-        return False
-    for palavra in range(0, len(strarr), k):
-        junto = strarr[palavra]
+        return ''
+    for palavra in range(0, len(strarr)):
+        if passo == len(strarr):
+            break
+        if k >= 2:
+            lista_palavras_juntas.append(strarr[palavra] + strarr[passo])
+            passo += 1
+        else:
+            lista_palavras_juntas.append(strarr[palavra])
 
 
-print(longest_consec(["zone", "abigail", "theta", "form", "libe", "zas"], 2))
+print(longest_consec(["ejjjjmmtthh", "zxxuueeg", "aanlljrrrxx", "dqqqaaabbb", "oocccffuucccjjjkkkjyyyeehh"], 1))
