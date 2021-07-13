1f01e5e3260e56a3e3d3b71523d284aa6fa234f5
Guilherme Santos <guipinela.santos@gmail.com>
2021-07-13 16:52:58 +0100

    commit

---
+++ 22.py
@@ -0,0 +1,9 @@
+def filter_list(l):
+    list_number = []
+    for i in l:
+        if type(i) == int:
+            list_number.append(i)
+    return list_number
+
+
+print(filter_list([1,2,'aasf','1','123',123]))
