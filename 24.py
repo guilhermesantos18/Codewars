a3c7115b622cea3ff03c36eba262f14c979fdf95
Guilherme Santos <guipinela.santos@gmail.com>
2021-07-15 14:01:24 +0100

    commit

---
+++ 24.py
@@ -0,0 +1,23 @@
+def multiplication_table(row, col):
+    lista = []
+    cont = 1
+    cont_pos = 0
+    mult = 2
+    for i in range(row):
+        lista.append([] * i)
+    for pos in range(1, col * row + 1):
+        if pos <= col:
+            lista[0].append(pos)
+        if len(lista[0]) == col:
+            lista[cont].append(lista[0][cont_pos]*mult)
+            cont_pos += 1
+            if col == len(lista[cont]):
+                cont += 1
+                cont_pos = 0
+                mult += 1
+            if len(lista[row-1]) == col:
+                break
+    return lista
+
+
+print(multiplication_table(3, 3))
