22cb8c3ab8b61ec6d73db3cd1c098be1af6676ab
Guilherme Santos <guipinela.santos@gmail.com>
2021-07-16 20:22:32 +0100

    commit

---
+++ 25.py
@@ -0,0 +1,15 @@
+def step(g, m, n):
+    lista = []
+    seg = cont = 0
+    for num in range(m, n + 1):
+        if cont == 0:
+            if num % g == float:
+                seg = num + g
+                cont += 1
+        if seg % 2 == float:
+            lista.append(num)
+            lista.append(seg)
+        else:
+            continue
+
+step(2,100,110)
