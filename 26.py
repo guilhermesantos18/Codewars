fbd2ca5c5b817b3647ea46c75d320c93348d6d9b
Guilherme Santos <guipinela.santos@gmail.com>
2021-07-20 16:54:00 +0100

    commit

--- 26.py
+++ 26.py
@@ -1,21 +1,20 @@
 def ip_to_int32(ip):
-    binary = ''
+    cont = 0
+    binary = '...'
+    binary = binary.split('.')
     ip = ip.split('.')
     for num in ip:
-        binary += bin(int(num))
-    binary = binary.replace('0b', '', )
-    print(binary)
-    # comprimento = len(binary) - 1
-    # cont = res = 0
-    # for i in binary:
-    #     i = int(i)
-    #     if i > 0:
-    #         dec = 2 ** (comprimento - cont)
-    #         cont += 1
-    #         res += dec
-    #     else:
-    #         cont += 1
-    # return res
-
-
-print(ip_to_int32('128.32.10.1'))
+        binary[cont] += bin(int(num))[2:].zfill(8)
+        cont += 1
+    binary = ''.join(binary)
+    comprimento = len(binary) - 1
+    cont = res = 0
+    for i in binary:
+        i = int(i)
+        if i > 0:
+            dec = 2 ** (comprimento - cont)
+            cont += 1
+            res += dec
+        else:
+            cont += 1
+    return res
