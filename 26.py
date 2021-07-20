d2bdccec181299f0d93772c2495c7942ed028a00
Guilherme Santos <guipinela.santos@gmail.com>
2021-07-20 16:54:45 +0100

    commit

--- 26.py
+++ 26.py
@@ -18,3 +18,9 @@ def ip_to_int32(ip):
         else:
             cont += 1
     return res
+
+# def ip_to_int32(ip):
+#     return int(''.join([(bin(int(x))[2:]).zfill(8) for x in ip.split('.')]), 2)
+#
+#
+# print(ip_to_int32('128.32.10.1'))
