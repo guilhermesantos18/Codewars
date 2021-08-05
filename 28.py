b08cb092d9a432c7b0fdda8d7ae4f158a3a14058
Guilherme Santos <guipinela.santos@gmail.com>
2021-08-05 12:53:15 +0100

    commit

--- 28.py
+++ 28.py
@@ -1,11 +1,25 @@
 def maze_runner(maze, directions):
-    end = maze[2][6]
-    start = maze[6][2]
-    caminho = []
-    cont_n = cont_s = cont_e = cont_w = 0
+    starty = startx = 0
+    for y in range(len(maze)):
+        for x in range(len(maze)):
+            if maze[y][x] == 2:
+                starty = y
+                startx = x
     for direc in directions:
-
-
+        if direc == 'N':
+            starty -= 1
+        elif direc == 'S':
+            starty += 1
+        elif direc == 'E':
+            startx += 1
+        elif direc == 'W':
+            startx -= 1
+    if maze[starty][startx] == 3:
+        return 'Finish'
+    elif maze[starty][startx] == 1 or maze[starty][startx] == :
+        return 'Dead'
+    else:
+        return 'Lost'
 
 
 maze = [[1, 1, 1, 1, 1, 1, 1],
@@ -15,4 +29,5 @@ maze = [[1, 1, 1, 1, 1, 1, 1],
         [1, 0, 1, 0, 1, 0, 1],
         [1, 0, 0, 0, 0, 0, 1],
         [1, 2, 1, 0, 1, 0, 1]]
-maze_runner(maze, ["N", "N", "N", "N", "N", "E", "E", "E", "E", "E"])
+
+print(maze_runner(maze, ["N","N","N","N","N","E","E","S","S","S","S","S","S"]))
