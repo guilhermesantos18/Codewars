3c426e8317cf62005cafe28d3f8d2a9ed77bb9f9
Guilherme Santos <guipinela.santos@gmail.com>
2021-08-19 23:00:11 +0100

    commit

--- 31.py
+++ 31.py
@@ -1,14 +1,26 @@
 def solution(s):
     lista_pos_maiuscula = []
     lista_palavras = []
-    pos_maiuscula = 0
-    for letra in s:
-        if letra.isupper():
-            lista_pos_maiuscula.append(s.index(letra))
+    cont_maiusculas = 0
+    for letra in range(len(s)):
+        if s[letra].isupper():
+            lista_pos_maiuscula.append(letra)
+            cont_maiusculas += 1
     for i in lista_pos_maiuscula:
-        lista_palavras.append(s[:i])
-        lista_palavras.append(s[i:])
-    print(' '.join(lista_palavras))
+        # if cont_maiusculas == 1:
+        #     if i == lista_pos_maiuscula[0]:
+        #         lista_palavras.append(s[:i])
+        #         lista_palavras.append(s[i:])
+        # elif cont_maiusculas >= 1:
+        #     if i == lista_pos_maiuscula[0]:
+        #         lista_palavras.append(s[:i])
+        #     elif i == lista_pos_maiuscula[-1]:
+        #         lista_palavras.append(s[i:])
+        #     else:
+        lista_palavras.append(s[i:i+1])
+    print(lista_palavras)
+        # lista_palavras.append(s[:i])
+        # lista_palavras.append(s[i:])
 
 
-solution('helloWorld')
+solution('breakCamelCase')
