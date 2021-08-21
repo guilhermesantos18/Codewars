24e662668ae5b079516629c93e0f393974fa2251
Guilherme Santos <guipinela.santos@gmail.com>
2021-08-21 13:09:18 +0100

    commit

--- 31.py
+++ 31.py
@@ -5,22 +5,21 @@ def solution(s):
     for letra in range(len(s)):
         if s[letra].isupper():
             lista_pos_maiuscula.append(letra)
-            cont_maiusculas += 1
+    print(lista_pos_maiuscula)
     for i in lista_pos_maiuscula:
-        # if cont_maiusculas == 1:
-        #     if i == lista_pos_maiuscula[0]:
-        #         lista_palavras.append(s[:i])
-        #         lista_palavras.append(s[i:])
-        # elif cont_maiusculas >= 1:
-        #     if i == lista_pos_maiuscula[0]:
-        #         lista_palavras.append(s[:i])
-        #     elif i == lista_pos_maiuscula[-1]:
-        #         lista_palavras.append(s[i:])
-        #     else:
-        lista_palavras.append(s[i:i+1])
+        if i == lista_pos_maiuscula[0]:
+            lista_palavras.append(s[:i])
+        elif i == lista_pos_maiuscula[-1]:
+            lista_palavras.append(s[i:])
+        else:
+            # if i > lista_pos_maiuscula[0] and i < lista_pos_maiuscula[-2]:
+            #     lista_palavras.append(s[i:])
+            if i == lista_pos_maiuscula[1]:
+                print(i)
+                lista_palavras.append(s[lista_pos_maiuscula[0]:i])
+            elif i == lista_pos_maiuscula[-2]:
+                lista_palavras.append(s[i:lista_pos_maiuscula[-1]])
     print(lista_palavras)
-        # lista_palavras.append(s[:i])
-        # lista_palavras.append(s[i:])
 
 
-solution('breakCamelCase')
+solution('usePointPublicThinkGreat')
