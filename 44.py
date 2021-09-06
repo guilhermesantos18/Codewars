1e89d726f0fd0e8a38c0e4a605f89bc048f70d86
guilhermesantos18 <guipinela.santos@gmail.com>
2021-09-06 22:26:04 +0100

    commit

--- 44.py
+++ 44.py
@@ -1,8 +1,16 @@
 def highest_rank(arr):
     lista_cont_num = []
-    lista_num = []
+    lista_nums = []
+    cont = 0
     for num in arr:
-        lista_cont_num.append(arr.count(num))
+        lista_cont_num.append(str(arr.count(num)))
+    maior_num = max(lista_cont_num)
+    for i in lista_cont_num:
+        if i == maior_num:
+            print(i)
+            lista_nums.append(arr[lista_cont_num.index(i) + cont])
+            cont += 1
+    return max(lista_nums)
 
 
-print(highest_rank([12, 10, 8, 8, 14, 14, 14, 14, 2, 4, 10, 12, 10, 10]))
+print(highest_rank([12, 10, 8, 12, 7, 6, 4, 10, 10]))
