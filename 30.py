674683532293303e4db6cc5c52c2069089490b29
Guilherme Santos <guipinela.santos@gmail.com>
2021-09-17 18:29:38 +0100

    commit

--- 30.py
+++ 30.py
@@ -2,9 +2,7 @@ def decipher_this(string):
     string = string.split(' ')
     for palavra in string:
         if palavra.isdigit():
-            print(chr(int(palavra)))
-
-    print(string[1][2])
+            chr(int(palavra))
 
 
 decipher_this('65 119esi 111dl 111lw 108dvei 105n 97n 111ka')
