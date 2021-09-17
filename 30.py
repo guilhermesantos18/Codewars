ca3eb2e2bb43baeaf3a82c2e148732cac4ce7d2c
Guilherme Santos <guipinela.santos@gmail.com>
2021-09-17 19:03:38 +0100

    commit

--- 30.py
+++ 30.py
@@ -1,8 +1,14 @@
 def decipher_this(string):
     string = string.split(' ')
+    num = ''
     for palavra in string:
         if palavra.isdigit():
             chr(int(palavra))
+        elif palavra.isalnum():
+            for letra in palavra:
+                if letra.isnumeric():
+                    num += letra
+            num = ''
 
 
 decipher_this('65 119esi 111dl 111lw 108dvei 105n 97n 111ka')
