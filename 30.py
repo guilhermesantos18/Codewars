e57dd2a9c736c6e174208817045fdd563a60b083
Guilherme Santos <guipinela.santos@gmail.com>
2021-09-22 15:25:16 +0100

    commit

--- 30.py
+++ 30.py
@@ -1,14 +1,24 @@
-def decipher_this(string):
+def encrypt_this(string):
     string = string.split(' ')
     num = ''
+    lista_palavras = []
+    comprimento_numero = 0
     for palavra in string:
         if palavra.isdigit():
-            chr(int(palavra))
+            lista_palavras.append(chr(int(palavra)))
         elif palavra.isalnum():
             for letra in palavra:
                 if letra.isnumeric():
                     num += letra
+                    comprimento_numero = len(num)
+            if num in palavra:
+                palavra = palavra.replace(palavra[0:comprimento_numero], chr(int(num)))
+                ultima_letra = palavra[-1]
+                palavra = palavra.replace(palavra[-1], palavra[1])
+                palavra = palavra.replace(palavra[1], ultima_letra, 1)
+                lista_palavras.append(palavra)
             num = ''
+    return ' '.join(lista_palavras)
 
 
-decipher_this('65 119esi 111dl 111lw 108dvei 105n 97n 111ka')
+print(encrypt_this('84eh 109ero 104e 115wa 116eh 108sse 104e 115eokp'))
