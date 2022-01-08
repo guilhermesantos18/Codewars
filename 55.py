8bf2d8299ba7edf04c611507344877087a1bb13a
Guilherme Santos <guipinela.santos@gmail.com>
2022-01-08 15:01:43 +0000

    commit

--- 55.py
+++ 55.py
@@ -1,29 +1,38 @@
 def increment_string(strng):
-    lista_letra = []
+    comprimento = len(strng)
+    lista_index = []
     lista_numeros = []
     cont_0 = 0
-    for letra in strng:
-        if letra.isnumeric():
-            lista_numeros.append(letra)
-            if letra == '0':
-                cont_0 += 1
-        else:
-            lista_letra.append(letra)
     if strng.isalpha():
-        letras = ''.join(lista_letra)
-        return letras + '1'
-    elif strng.isalnum() or strng.isprintable() and not strng == '':
-        num = int(''.join(lista_numeros))
-        if num == 0:
+        return strng + '1'
+    elif strng.isalnum():
+        for l in strng:
+            if l.isnumeric():
+                lista_numeros.append(l)
+                if l == '0':
+                    cont_0 += 1
+        num = ''.join(lista_numeros)
+        strng = strng.replace(num, '')
+        if num == '00':
             num = int(''.join(lista_numeros)) + 1
             num = str(num).zfill(cont_0)
         else:
             num = int(''.join(lista_numeros)) + 1
-            num = str(num).zfill(cont_0 + 1)
-        letras = ''.join(lista_letra)
-        return letras + num
-    else:
+            num = str(num).zfill(comprimento)
+        return strng + num
+    elif strng == '':
         return '1'
+    elif strng.isprintable():
+        for index, letra in enumerate(strng):
+            if str(letra).isalpha():
+                lista_index.append(index)
+    number = strng[lista_index[-1] + 1:]
+    for i in number:
+        if i in '!"@#£$§%&/{([)]=}?*+^~_-.,;:<»>|':
+            number = number.replace(i, '')
+    strng = strng.replace(number, '')
+    number = int(number) + 1
+    return strng + str(number)
 
 
-print(increment_string(''))
+print(increment_string('0000000731929'))
